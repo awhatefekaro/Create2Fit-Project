555099cc3a99e1b173b8a0d1671e0bc25ae39f4c
O'Karo Awhatefe <awhatefekaro@yahoo.com>
2024-10-24 12:46:14 +0100

    Updated the conted of user.sh

---
+++ user.sh
@@ -0,0 +1,7 @@
+changed this file name from user,sh to user.sh
+tried to commit it like a modified file and it failed.
+lets try it now that we have modified the file
+git commit -a -m
+am sure it would work now perfecting...
+lets see if the working tree is going to m=be clean 
+by the above command.
